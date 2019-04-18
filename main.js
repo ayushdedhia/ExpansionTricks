669b34256128b4e94b60d626a57a6e1520a24ec6
Ayush Dedhia <ayushdedhia25@gmail.com>
2019-04-17 19:38:39 -0700

    Hello-Genius
    Hello-Genius

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 $(document).ready(function(){
+
+    var loader = $('#preLoader');
+
+    function preLoad() {
+        loader.style.display = "none";
+    }
+
+    var userName = $('#user');
+    var password = $('#password');
+    var confirmPassword = $('#con-password');
+    var button = $('#btn');
+    var modal = $('#myModal');
     
     myFunction = function(){
 
-        var userName = $('#user');
-        var password = $('#password');
-        var confirmPassword = $('#con-password');
-        var button = $('#btn');
-        var modal = $('#myModal');
-
         if ( userName.val() !== "" && password.val() !== "" && confirmPassword.val() !== "" ) {
-            
+
             if ( password.val() == confirmPassword.val() ) {
+
+                if ( password.val() == "admin" ) {
+
+                    alert('Initializing the Hacking Hub');
+
+                } else {
+
+                    alert('Failed to Initialize');
+
+                }
 
                 button.attr({
                     "data-toggle": "modal",
@@ -18,13 +34,9 @@ $(document).ready(function(){
                 });
 
                 changeData = function() {
-
                     $('#emptyFields h5').addClass('text-success').text("Success");
-
                     $('#emptyFields #m-body').addClass('text-capitalize').text("Your account was successfully verified!");
-
                     $('.modal-footer button').removeClass('btn-secondary').addClass('btn-success');
-
                 }
 
                 changeData();
@@ -35,8 +47,8 @@ $(document).ready(function(){
 
             } else {
 
+                alert('Sorry No Match Found');
 
-                
             }
 
         } else {
@@ -53,4 +65,6 @@ $(document).ready(function(){
     $('#myTable tbody tr td:nth-child(5)').prepend('+91 ');
     $('#myTable tbody tr td').addClass('align-middle');
 
+
 });
+
